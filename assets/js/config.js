ea9b52a797ec2e83f61881f4dbd2669de06c661e
keicha2025 <wj209ing@gmail.com>
2025-12-24 12:46:02 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,8 +1,22 @@
-// assets/js/config.js
 const KEICHA_CONFIG = {
+    siteName: "KEICHA",
     lineId: "@366qwylw",
     lineLink: "https://lin.ee/QJU5mUO",
-    pcPayLink: "https://pcpay.tw/NF4vv",
-    myshipLink: "https://myship.7-11.com.tw/cart/easy/GM2510186847800",
-    email: "keicha.nihoncha@gmail.com"
+    contactEmail: "keicha.nihoncha@gmail.com",
+    paymentLink: "https://pcpay.tw/NF4vv", // PChomePay 萬用連結
+    
+    // 導覽選單配置
+    navLinks: [
+        { title: "抹茶代購狀況", url: "./maccha.html" },
+        { title: "電話代播服務", url: "./denwa.html" },
+        { title: "訂單追蹤", url: "./jyoukyou.html" },
+        { title: "貨到付款下單", url: "./diy.html" },
+    ],
+    
+    // 頁尾資訊
+    footer: {
+        copyRight: "© 2025 KEICHA. All Rights Reserved.",
+        shippingInfo: "預購商品下單後約 15~20 天內出貨。",
+        refundPolicy: "預購商品不適用猶豫期，恕不接受取消或退貨。"
+    }
 };
